cf64788e12428551ffb181b3b2df5d021e39639e
zhangpengzhi <zhang_pz@suixingpay.com>
2018-02-26 14:57:26 +0800

    地址修改

--- js/page/config.js
+++ js/page/config.js
@@ -1,2 +1,2 @@
 axios.defaults.headers.common['Authorization'] = (sessionStorage.getItem("Authorization") == null)?localStorage.getItem("Authorization"):sessionStorage.getItem("Authorization");
-axios.defaults.baseURL = "http://localhost:8080"
+axios.defaults.baseURL = "http://106.15.183.161:8080"
