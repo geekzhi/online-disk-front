d37892ac403bbea93890476e7ced69da009ea9af
zhangpengzhi <zhang_pz@suixingpay.com>
2018-02-27 20:03:09 +0800

    1

--- js/page/my.js
+++ js/page/my.js
@@ -1,13 +1,13 @@
 $(function () {
-    // axios.post('/user/username').then(function (value) {
-    //     if ((value.data == '')) {
-    //         location.href = 'login.html';
-    //     } else {
-    //         $('#username').html("Hello," + value.data);
-    //     }
-    // }).catch(function (reason) {
-    //     location.href = 'login.html';
-    // });
+    axios.post('/user/username').then(function (value) {
+        if ((value.data == '')) {
+            location.href = 'login.html';
+        } else {
+            $('#username').html("Hello," + value.data);
+        }
+    }).catch(function (reason) {
+        location.href = 'login.html';
+    });
 
     $('#logout').click(function () {
         axios.get("/user/logout").then(function (value) {
